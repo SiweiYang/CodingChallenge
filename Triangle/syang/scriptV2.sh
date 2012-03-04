05fa7c38d28303f8505c5a6957ecc7b4e44c4357
Siwei Yang <yangsiwei880813@gmail.com>
2012-03-04 12:15:19 -0500

    tiny improvement on parallelism

--- Triangle/syang/scriptV2.sh
+++ Triangle/syang/scriptV2.sh
@@ -7,49 +7,43 @@ time ./HT-OPTIMIZED 0204 0300 +RTS -K100000000 -RTS >> result &
 time ./HT-OPTIMIZED 0304 0400 +RTS -K100000000 -RTS >> result
 echo [0400] >> result
 
-echo [0404,100,0700] >> result
+echo [0404,100,0800] >> result
 time ./HT-OPTIMIZED 0404 0500 +RTS -K100000000 -RTS >> result &
 time ./HT-OPTIMIZED 0504 0600 +RTS -K100000000 -RTS >> result &
-time ./HT-OPTIMIZED 0604 0700 +RTS -K100000000 -RTS >> result
-echo [0700] >> result
+time ./HT-OPTIMIZED 0604 0700 +RTS -K100000000 -RTS >> result &
+time ./HT-OPTIMIZED 0704 0800 +RTS -K100000000 -RTS >> result
+echo [0800] >> result
 
-echo [0704,100,1000] >> result
-time ./HT-OPTIMIZED 0704 0800 +RTS -K100000000 -RTS >> result &
+echo [0804,100,1200] >> result
 time ./HT-OPTIMIZED 0804 0900 +RTS -K100000000 -RTS >> result &
-time ./HT-OPTIMIZED 0904 1000 +RTS -K100000000 -RTS >> result
-echo [1000] >> result
-
-echo [1004,100,1300] >> result
+time ./HT-OPTIMIZED 0904 1000 +RTS -K100000000 -RTS >> result &
 time ./HT-OPTIMIZED 1004 1100 +RTS -K100000000 -RTS >> result &
-time ./HT-OPTIMIZED 1104 1200 +RTS -K100000000 -RTS >> result &
-time ./HT-OPTIMIZED 1204 1300 +RTS -K100000000 -RTS >> result
-echo [1300] >> result
+time ./HT-OPTIMIZED 1104 1200 +RTS -K100000000 -RTS >> result
+echo [1200] >> result
 
-echo [1304,100,1600] >> result
+echo [1204,100,1600] >> result
+time ./HT-OPTIMIZED 1204 1300 +RTS -K100000000 -RTS >> result &
 time ./HT-OPTIMIZED 1304 1400 +RTS -K100000000 -RTS >> result &
 time ./HT-OPTIMIZED 1404 1500 +RTS -K100000000 -RTS >> result &
 time ./HT-OPTIMIZED 1504 1600 +RTS -K100000000 -RTS >> result
 echo [1600] >> result
 
-echo [1604,100,1900] >> result
+echo [1604,100,2000] >> result
 time ./HT-OPTIMIZED 1604 1700 +RTS -K100000000 -RTS >> result &
 time ./HT-OPTIMIZED 1704 1800 +RTS -K100000000 -RTS >> result &
-time ./HT-OPTIMIZED 1804 1900 +RTS -K100000000 -RTS >> result
-echo [1900] >> result
+time ./HT-OPTIMIZED 1804 1900 +RTS -K100000000 -RTS >> result &
+time ./HT-OPTIMIZED 1904 2000 +RTS -K100000000 -RTS >> result
+echo [2000] >> result
 
-echo [1904,100,2200] >> result
-time ./HT-OPTIMIZED 1904 2000 +RTS -K100000000 -RTS >> result &
+echo [2004,100,2400] >> result
 time ./HT-OPTIMIZED 2004 2100 +RTS -K100000000 -RTS >> result &
-time ./HT-OPTIMIZED 2104 2200 +RTS -K100000000 -RTS >> result
-echo [2200] >> result
-
-echo [2204,100,2500] >> result
+time ./HT-OPTIMIZED 2104 2200 +RTS -K100000000 -RTS >> result &
 time ./HT-OPTIMIZED 2204 2300 +RTS -K100000000 -RTS >> result &
-time ./HT-OPTIMIZED 2304 2400 +RTS -K100000000 -RTS >> result &
-time ./HT-OPTIMIZED 2404 2500 +RTS -K100000000 -RTS >> result
-echo [2500] >> result
+time ./HT-OPTIMIZED 2304 2400 +RTS -K100000000 -RTS >> result
+echo [2400] >> result
 
-echo [2504,100,2800] >> result
+echo [2404,100,2800] >> result
+time ./HT-OPTIMIZED 2404 2500 +RTS -K100000000 -RTS >> result &
 time ./HT-OPTIMIZED 2504 2600 +RTS -K100000000 -RTS >> result &
 time ./HT-OPTIMIZED 2604 2700 +RTS -K100000000 -RTS >> result &
 time ./HT-OPTIMIZED 2704 2800 +RTS -K100000000 -RTS >> result
