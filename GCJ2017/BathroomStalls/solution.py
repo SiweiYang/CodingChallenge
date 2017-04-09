2f0143bc2c933bf76c61864b2a2d746166d5bd4c
Siwei Yang <Siwei Yang>
2017-04-08 21:08:56 -0400

    have to use python on windows machine...

---
+++ GCJ2017/BathroomStalls/solution.py
@@ -0,0 +1,26 @@
+import click
+import math
+
+def find_depth(k, depth=0):
+    if k < 2**depth:
+        return depth
+    return find_depth(k, depth + 1)
+
+num = int(input())
+for i in range(num):
+    N, K = map(int, input().split())
+    
+    depth = find_depth(K) - 1
+    # print(depth)
+    occupied = 2**depth - 1
+    # print(occupied)
+    stall_left = N - occupied
+    user_left = K - occupied
+    size = stall_left // 2**depth
+    # print(size)
+    residule = stall_left - size * 2**depth
+    # print(residule)
+    if not user_left > residule:
+        size += 1
+    # print(size)
+    print('Case #{}: {} {}'.format(i + 1, math.ceil((size - 1) / 2), math.floor((size - 1) / 2)))
